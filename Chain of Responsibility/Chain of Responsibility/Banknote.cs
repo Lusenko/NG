d297b519486ba18bf4fcab4e94e09acba039b1e2
Oleksandr Lusenko <60522005+Lusenko@users.noreply.github.com>
2021-02-05 22:35:49 +0200

    I added project

---
+++ Chain of Responsibility/Chain of Responsibility/Banknote.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patern2
+{
+    class Banknote
+    {
+        public Banknote(int faceValue, int requiredAmnt = 0)
+        {
+            FaceValue = faceValue;
+            RequiredAmnt = requiredAmnt;
+        }
+        public int FaceValue { get; private set; }
+        public int RequiredAmnt { get; private set; }
+        override public string ToString()
+        {
+            return "Номинал: " + FaceValue + ",\tнеобходимое количество: " + RequiredAmnt;
+        }
+    }
+}
---
+++ Chain of Responsibility/Chain of Responsibility/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patern2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сумму: ");
+            int sum;
+            while (!Int32.TryParse(Console.ReadLine(), out sum) || sum <= 0)
+            {
+                Console.WriteLine("Сумма должна быть натуральным числом попробуйте еще раз:");
+            }
+
+            int[] faceValues = { 50, 20, 10, 5, 2, 1, };
+            List<Banknote> banknotes = new List<Banknote>();
+            foreach (int nominal in faceValues)
+            {
+                if (nominal <= sum)
+                {
+                    banknotes.Add(new Banknote(faceValue: nominal, requiredAmnt: sum / nominal));
+                    sum %= nominal;
+                }
+            }
+
+           banknotes.ForEach(Console.WriteLine);
+            Console.ReadLine();
+        }
+        
+    }
+}
