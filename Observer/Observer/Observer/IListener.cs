05b096c8cad2476a02d25b38f201fcc7cfe14924
Oleksandr Lusenko <60522005+Lusenko@users.noreply.github.com>
2021-02-24 19:24:24 +0200

    changes List on Dictoinary in EventManager

---
+++ Observer/Observer/Observer/IListener.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    interface IListener
+    {
+        void Update(Job job, string eventName);
+    }
+}
